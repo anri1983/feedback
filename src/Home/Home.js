d9fced6aec2dab1584d6b8db32cbbc4a4e349f7c
anri1983 <81773528+anri1983@users.noreply.github.com>
2022-12-05 23:15:08 +0300

    Change H1

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
  function Home() {
-    return <> <h1 style = {{textAlign: 'center'}}>Home</h1>
-<h2>Привет Марина</h2>
+    return <> <h1 style = {{textAlign: 'center'}}>Hello Marina</h1>
     <div style = {{textAlign: 'center', fontSize: "200px" }}>&#9883;</div>
     </>
 }
