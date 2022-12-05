76a618a02127dcc5f5eb216e387f6b8c4e923a2b
anri1983 <81773528+anri1983@users.noreply.github.com>
2022-12-05 23:08:49 +0300

    h2 add

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
  function Home() {
     return <> <h1 style = {{textAlign: 'center'}}>Home</h1>
+<h2>Привет Марина</h2>
     <div style = {{textAlign: 'center', fontSize: "200px" }}>&#9883;</div>
     </>
 }
